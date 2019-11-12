a544c5c236b5212485b0d749c1863fa56ff53856
vinso <jakub.vins2@gmail.com>
2019-11-12 20:19:37 +0100

    Added the rest of tables

--- script.sql
+++ script.sql
@@ -1,7 +1,8 @@
-drop table restaurantHasItem;
-drop table itemWillBeInMenu;
-drop table restaurant;
-drop table item;
+# drop table restaurantHasItem;
+# drop table itemWillBeInMenu;
+# drop table restaurant;
+# drop table item;
+drop table `Order`;
 
 
 CREATE TABLE restaurant (
@@ -19,7 +20,7 @@ CREATE TABLE item (
   description VARCHAR(50),
   picture VARCHAR(50),
   price INT,
-  type ENUM("dailyMenu", "sauce", "meal", "beverage"),
+  type ENUM("dailyMenu", "sauce", "meal", "beverage","sidedish"),
   isInMenu BOOLEAN DEFAULT FALSE,
   isVegan BOOLEAN DEFAULT FALSE,
   isGlutenFree BOOLEAN DEFAULT FALSE
@@ -49,6 +50,29 @@ DO
   update item natural join itemWillBeInMenu set isInMenu = true
   where date = CURRENT_DATE();
 
+CREATE TABLE Person (
+  personId INT NOT NULL PRIMARY KEY,
+  Name VARCHAR(50),
+  Town VARCHAR(50),
+  Street VARCHAR(50),
+  ZIP INT,
+  isRegistered BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE Driver (
+  driverId INT NOT NULL PRIMARY KEY,
+  FOREIGN KEY (driverId) REFERENCES Person(personId)
+);
+
+
+CREATE TABLE `Order` (
+  orderId INT NOT NULL PRIMARY KEY,
+  state ENUM("unconfirmed", "confirmed", "delivery"),
+  dinerId INT NOT NULL,
+  driverId INT NOT NULL,
+  FOREIGN KEY (dinerId) REFERENCES Person(personId),
+  FOREIGN KEY (driverId) REFERENCES Driver(driverId)
+);
 
 
 insert into restaurant values (1, "purkynka", "brno", "purkynova", 61200, TIME("13:10:11"));
@@ -63,6 +87,14 @@ insert into restaurantHasItem values (1, 102);
 insert into itemWillBeInMenu values (102, DATE("2019-11-10"));
 insert into itemWillBeInMenu values (103, DATE("2019-11-11"));
 insert into itemWillBeInMenu values (104, DATE("2019-11-12"));
+
+
+insert into Person values (10, "Jakub", "brno", "purkynova", 61200, true);
+insert into Person values (11, "Dominik", "brno", "purkynova", 61200, true);
+insert into Driver values (11);
+
+
+insert into `Order` values (20, "unconfirmed", 10, 11);
 
 
 select * from restaurant;
