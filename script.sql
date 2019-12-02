af7702cdbbca10fdac0a41c3db32741e9d7a93dd
vinso <jakub.vins2@gmail.com>
2019-12-02 11:31:04 +0100

    Modifying example data

--- script.sql
+++ script.sql
@@ -79,37 +79,37 @@ CREATE TABLE orderHasItem (
 
 
 
-insert into restaurant values (1, "purkynka", "some description", "brno", "purkynova", 61200, "0944456789", TIME("08:00:00"), TIME("18:00:00"));
-insert into restaurant values (2, "skacelka", "some description", "brno", "purkynova", 61200, "0944456789", TIME("09:00:00"), TIME("21:30:00"));
-insert into restaurant values (3, "forkys", "some description", "Trnava", "purkynova", 61200, "0944456789", TIME("07:00:00"), TIME("17:00:00"));
-insert into restaurant values (4, "forkysq", "some description", "Trnava", "purkynova", 61200, "0944456789", TIME("07:00:00"), TIME("17:00:00"));
-insert into restaurant values (5, "forkysw", "some description", "Trnava", "purkynova", 61200, "0944456789", TIME("07:00:00"), TIME("17:00:00"));
-insert into restaurant values (6, "forkyse", "some description", "Trnava", "purkynova", 61200, "0944456789", TIME("07:00:00"), TIME("17:00:00"));
+insert into restaurant values (1, "Purkynka", "Menza", "Brno", "Purkynova", 61200, "0944456789", TIME("08:00:00"), TIME("18:00:00"));
+insert into restaurant values (2, "Skacelka", "some description", "Brno", "Purkynova", 61200, "0944456789", TIME("09:00:00"), TIME("21:30:00"));
+insert into restaurant values (3, "Forkys", "Vegetarian restaurant", "Trnava", "Purkynova", 61200, "0944456789", TIME("07:00:00"), TIME("17:00:00"));
+insert into restaurant values (4, "Lokofu", "Chinese restaurant", "Brno-stred", "Kounicova 966", 60200, "0944456789", TIME("07:00:00"), TIME("17:00:00"));
+insert into restaurant values (5, "Running & sushi", "Sushi sushi", "Trnava", "Kollarova 555", 91701, "0944456789", TIME("07:00:00"), TIME("20:00:00"));
+insert into restaurant values (6, "Pizza Piccolino Trnava", "some description", "Trnava", "Hlavna 14", 91701, "0944456789", TIME("07:00:00"), TIME("17:00:00"));
 
 
-insert into item values (100, "Spaghetti and Meatballs", "Delicious spaghetti", "uploads/spageti.jpg", 8.2, "dailyMenu", true, false);
-insert into item values (101, "lasagne", "lasagne decrip", "uploads/lasagne.jpg", 6.3, "dailyMenu", false, false);
-insert into item values (102, "chilli con carne", "lasagne decrip", "uploads/chilli.jpg", 4.4, "dailyMenu", true, false);
+insert into item values (100, "Spaghetti and Meatballs", "Delicious spaghetti", "uploads/spageti.jpg", 8.2, "dailyMenu", false, false);
+insert into item values (101, "Lasagne", "Very tasty lasagne", "uploads/lasagne.jpg", 6.3, "dailyMenu", false, false);
+insert into item values (102, "Chilli con carne", "Very nice chilli", "uploads/chilli.jpg", 4.4, "dailyMenu", true, false);
 
-insert into item values (104, "spaghetti", "lasagne decrip", "uploads/spaghetiiplain.jpg", 3.4, "sidedish", false, false);
-insert into item values (105, "rice", "lasagne decrip", "uploads/rice.jpg", 2.7, "sidedish", false, false);
-insert into item values (106, "mashed potatoes", "lasagne decrip", "uploads/default.jpg", 3.6, "sidedish", false, false);
-insert into item values (107, "chips", "lasagne decrip", "uploads/default.jpg", 4.2, "sidedish", false, false);
+insert into item values (104, "Spaghetti", "Pasta di pizza", "uploads/spaghetiiplain.jpg", 3.4, "meal", false, false);
+insert into item values (105, "Rice", "Nice rice", "uploads/rice.jpg", 2.7, "sidedish", false, false);
+insert into item values (106, "Mashed potatoes", "Some tasty potatoes these are", "uploads/default.jpg", 3.6, "sidedish", false, false);
+insert into item values (107, "Chips", "Even tastier potatoes these ones", "uploads/default.jpg", 4.2, "sidedish", false, false);
 
-insert into item values (108, "majonese", "lasagne decrip", "uploads/default.jpg", 1.4, "sauce", false, false);
-insert into item values (109, "ketchup", "lasagne decrip", "uploads/default.jpg", 0.8, "sauce", false, false);
-insert into item values (110, "mustard", "lasagne decrip", "uploads/default.jpg", 0.9, "sauce", false, false);
-insert into item values (111, "lemon juice", "lasagne decrip", "uploads/default.jpg", 0.9, "sauce", false, false);
+insert into item values (108, "Majonese", "Majoneseee", "uploads/default.jpg", 1.4, "sauce", false, false);
+insert into item values (109, "Ketchup", "Poor tomatoes", "uploads/default.jpg", 0.8, "sauce", false, false);
+insert into item values (110, "Mustard", "Mustard description", "uploads/default.jpg", 0.9, "sauce", false, false);
+insert into item values (111, "Lemon juice", "Freshly squeezed", "uploads/default.jpg", 0.9, "sauce", false, false);
 
-insert into item values (112, "Coca cola", "lasagne decrip", "uploads/default.jpg", 2.4, "beverage", false, false);
-insert into item values (113, "Beer", "lasagne decrip", "uploads/default.jpg", 3.2, "beverage", false, false);
-insert into item values (114, "Wine", "lasagne decrip", "uploads/default.jpg", 1.6, "beverage", false, false);
-insert into item values (115, "Soda", "lasagne decrip", "uploads/default.jpg", 0.5, "beverage", false, false);
+insert into item values (112, "Coca cola", "Sizzlin", "uploads/default.jpg", 2.4, "beverage", false, false);
+insert into item values (113, "Beer", "Bubblin", "uploads/default.jpg", 3.2, "beverage", false, false);
+insert into item values (114, "Wine", "South slope", "uploads/default.jpg", 1.6, "beverage", false, false);
+insert into item values (115, "Soda", "Sizzlin n bubblin", "uploads/default.jpg", 0.5, "beverage", false, false);
 
-insert into item values (116, "grilled chicken", "lasagne decrip", "uploads/chicken.jpg", 7.1, "meal", false, true);
-insert into item values (117, "salmon", "lasagne decrip", "uploads/salnom.jpg", 8.3, "meal", false, false);
-insert into item values (118, "pork", "lasagne decrip", "uploads/pork.jpg", 9.7, "meal", false, false);
-insert into item values (119, "pizza", "lasagne decrip", "uploads/pizza.jpg", 5.4, "meal", false, true);
+insert into item values (116, "Grilled chicken", "Very nice and tasty", "uploads/chicken.jpg", 7.1, "meal", false, true);
+insert into item values (117, "Salmon", "Very nice", "uploads/salnom.jpg", 8.3, "meal", false, false);
+insert into item values (118, "Pork", "Very tasty", "uploads/pork.jpg", 9.7, "meal", false, false);
+insert into item values (119, "Pizza", "Pizza di pasta", "uploads/pizza.jpg", 5.4, "meal", false, true);
 
 
 insert into restaurantHasItem values (1, 100);
@@ -136,18 +136,19 @@ insert into restaurantHasItem values (1, 117);
 insert into restaurantHasItem values (1, 118);
 insert into restaurantHasItem values (1, 119);
 
-insert into person values (10, "Jakub", "someSUrname", "brno", "purkynova", 61200, "0985456789", "jakub@gmail.com", "hashedPassword", "admin");
-insert into person values (11, "Dominik", "someSUrname", "brno", "purkynova", 61200, "0985456789", "w@w", "$2y$10$PqBa4hPSv2xdY4.XNtQvYuuyTh0YkYPzwGo2WWSdscnNFzr9HqLcm", "admin");
-insert into person values (12, "Peter", "someSUrname", "Jaslovske Bohunice", "Atomka", 61200, "0985456789", "peter@gmail.com", "hashedPassword", "diner");
-insert into person values (13, "Marek", "someSUrname", "Trnava", "Skusobka", 61200, "0985456789", "marek@gmail.com", "hashedPassword", "diner");
-insert into person values (14, "Michal", "someSUrname", "Trnava", "Johna Dopieru 26/a", 61200, "0985456789", "michal@gmail.com", "hashedPassword", "driver");
+insert into person values (10, "Jakub", "someSurname", "brno", "purkynova", 61200, "0985456789", "jakub@gmail.com", "hashedPassword", "admin");
+insert into person values (11, "Dominik", "someSurname", "brno", "purkynova", 61200, "0985456789", "w@w", "$2y$10$PqBa4hPSv2xdY4.XNtQvYuuyTh0YkYPzwGo2WWSdscnNFzr9HqLcm", "admin");
+insert into person values (12, "Admin", "adminSurname", "Jaslovske Bohunice", "Atomka", 61200, "0985456789", "admin@gmail.com", "$2y$10$J6zUNuhyNCV9uY/F/Yzlr.sh5/7Ejo2kaPvIC7GP8V3TvUrg54RFa", "admin");
+insert into person values (13, "Operator", "operatorSurname", "Trnava", "Skusobka", 61200, "0985456789", "operator@gmail.com", "$2y$10$yHLMJOAcCqXweWeaiU6IuuNzSGph7E7.ZX/7zcl/Rl.twKJe7Dw7K", "operator");
+insert into person values (14, "Driver", "driverSurname", "Trnava", "Johna Dopieru 26/a", 61200, "0985456789", "driver@gmail.com", "$2y$10$T3ib4zk/BCAifX7OleTRiurK1aoARJr6xHD2LgSjOJLcfQFbSyd0G", "driver");
+insert into person values (15, "Diner", "dinerSurname", "Brno", "Skacelova", 61200, "0985456789", "diner@gmail.com", "$2y$10$blyLUvZsCTITgNL3hxAXCO7j8EFa2f3R.y0iU6vV7ZsztJYEKWmx2", "diner");
 
 
-insert into `order` values (20, "Please call me 20min before delivery", "placedButNotAssignedToDriver", 10, null,1);
-insert into `order` values (21, "Please call me 20min before delivery", "placedButNotAssignedToDriver", 11, null,2);
-insert into `order` values (22, "Please call me 20min before delivery", "placedButNotAssignedToDriver", 12, null,3);
-insert into `order` values (23, "Please call me 20min before delivery", "placedButNotAssignedToDriver", 13, null,4);
-insert into `order` values (24, "Please call me 20min before delivery", "placedButNotAssignedToDriver", 14, null,5);
+# insert into `order` values (20, "Please call me 20min before delivery", "placedButNotAssignedToDriver", 10, null,1);
+# insert into `order` values (21, "Please call me 20min before delivery", "placedButNotAssignedToDriver", 11, null,2);
+# insert into `order` values (22, "Please call me 20min before delivery", "placedButNotAssignedToDriver", 12, null,3);
+# insert into `order` values (23, "Please call me 20min before delivery", "placedButNotAssignedToDriver", 13, null,4);
+# insert into `order` values (24, "Please call me 20min before delivery", "placedButNotAssignedToDriver", 14, null,5);
 
 
 -- select * from restaurant;
